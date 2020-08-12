3575429e2ac10e0f14db35aba1c7d501bc109c81
abdllhcay <abdllhcay@gmail.com>
2020-08-12 14:17:13 +0300

    Add project files.

---
+++ Blogger.Core/Entities/Post.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Blogger.Core.Entities
+{
+    public class Post
+    {
+        [Key]
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Summary { get; set; }
+        public DateTime PublishDate { get; set; }
+        public List<string> Tags { get; set; }
+        public int Category { get; set; }
+        public int Status { get; set; }
+    }
+}
---
+++ Blogger.Infrastructure/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Blogger.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blogger.Infrastructure.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Post> Posts { get; set; }
+    }
+}
